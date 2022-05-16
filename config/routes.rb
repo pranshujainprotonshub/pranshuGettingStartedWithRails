2095fe53d715c8c95371a321b861a44741a75564
pranshujainprotonshub <pranshu.jain@protonshub.in>
2022-05-16 11:05:51 +0530

    Getting Started With Rails

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # get "/articles" ,to "articles#index"
+  # get 'articles/show'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/delete'
+  resources :articles do 
+    resources :comments
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
